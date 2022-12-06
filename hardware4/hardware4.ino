5e4dadac841bf879d3acb78020182599b48cfdd6
ChrisMoller <moller@mollerware.com>
2022-12-06 12:09:14 -0500

    lots of comments

--- hardware4/hardware4.ino
+++ hardware4/hardware4.ino
@@ -1,21 +1,79 @@
+#if 0
+
+This code runs on two different computers simultaneously,
+the Arduino and some machine running a browser.  The Arduino
+side does three things, it serves a web page to the browser,
+which allows the user to set various parameters, it reads
+the parameters, and it interprets the parameters to control
+the servos.  The broswer side runs a web page with the controls.
+The Arduino side is coded in a subset of C++; the browser side
+is in a mix of Javascript and HTML.  In the code below,
+srgument strings to client.print () calls are the HTML and
+Javascript being served to the browser.
+					     
+#endif
+
 #include <SPI.h>
 #include <SD.h>
 #include <WiFiNINA.h>
 #include <JOAAT.h>
 
+/********** global variables ***********/
+
+/* An instantiation of a class that generates hashes */
 JOAAT joaat;
 
+/* An instantiation of a class that turns the Arduino into a web server */
+WiFiServer server(80);
+
+/* WiFi credentials */
 #define SECRET_SSID "NETGEAR80"
 #define SECRET_PASS "magical574"
-
 char ssid[] = SECRET_SSID;    // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password 
 
-int status = WL_IDLE_STATUS;
-
+/* holds th local ip address.  not sure i need it... */
 IPAddress ip;
 
-WiFiServer server(80);
+/******* parameter labels *******/
+
+#define ETY(v) #v
+const char * lbls[] = {
+#include "lbls.h"
+};
+size_t lbl_cnt = sizeof(lbls)/sizeof(char *);
+
+/******* parameter enums *******/
+
+#define ETY(v) PARM_ ## v
+enum {
+#include "lbls.h"
+};
+
+/*****
+      One of my rules is to never enter the same information more than once
+      --it's too easy for the different versions to get out of sync.  What the
+      above does is define the macro ETY two different ways and then include a
+      a file containing a list of parameter names for each of those ways.
+      Under the first definition, that file expands into a char * vector of
+      the names; under the second def it expands into a list of indices into
+      that vactor, and both lists are guaranteed to stay in sync.
+
+      A macro, BTW, (like where it says "#define ETY(v) #v" above) is a string-
+      manipulation thing.  The compiler works in a lot of different stages, but
+      the first one goes through the code looking for things like #define
+      statements and then, when it sees a string in the code that matches a
+      definition, ETY in this case, it plugs the argument, v in this case, into
+      the expansion ("#v" or "PARM_ ## v" in these cases) so entries in lbl.h,
+      e,g., ETY(pdx), get expanded into "pdx" or PARM_pdx respectively.  (The
+      actual expansion process is pretty complex--I won't go into it here.)
+      (The preprocessing stage also handles things like #if statements,
+      comment removal, and so on--a whole lot of stuff.)
+ *****/
+
+/******* parameter structure *********/
+/* collected information about each parameter, it's name, it's current value
+   and a hash value which will be explaned below.*/
 
 typedef struct {
   double val;
@@ -23,27 +81,14 @@ typedef struct {
   const char *name;
 } parm_s;
 
-const char * lbls[] = {
-  "pdx",
-  "pdy",
-  "pdz",
-  "proll",
-  "ppitch",
-  "pyaw",
-  "jdx",
-  "jdy",
-  "jdz",
-  "jroll",
-  "jpitch",
-  "jyaw",
-  "onset",
-  "relax",
-  "interval"
-};
-
-size_t lbl_cnt = sizeof(lbls)/sizeof(char *);
-
+/* A pointer to an unallocated array of the parameters. */
 parm_s *parms = nullptr;
+
+
+/******** subroutines and functions *********/
+
+
+/********* comparison function for qsort (see below) **********/
 
 int cmp_parm (void *s1, void * s2)
 {
@@ -53,6 +98,8 @@ int cmp_parm (void *s1, void * s2)
     : ((a < b) ? -1 : 1);
 }
 
+
+/********* comparison function for bsearch (see below) **********/
 
 int cmp_parm_str (const void *s1, const void *s2)
 {
@@ -72,6 +119,8 @@ int cmp_parm_str (const void *s1, const void *s2)
 }
 
 
+/********* utility function for qsort (see below) **********/
+
 void swap(void* v1, void* v2, int size)
 {
   char buffer[size];
@@ -79,6 +128,31 @@ void swap(void* v1, void* v2, int size)
   memcpy(v1, v2, size);
   memcpy(v2, buffer, size);
 }
+
+
+#if 0
+
+All the control parameters have names like "pdx."  In order to access or
+change the value of a parameter, it has to be looked up in the parameter
+array, which means matching the string containing the name against the names
+in the parameter array.  The "unsophisticated" programmer usually does this
+with a series of "if name = this, then do ..., else if name = that..." etc.
+Comparing strings is very slow and if you have a lot of names in the array
+all these compares take a linearly long time.  Comparing numbers is vastly
+faster so the trick is to turn name strings into signature numbers.  This is
+called "hashing."
+
+Further, there are faster ways of looking up numbers than by scanning a list,
+one of which is called a binary search.  If the list is sorted by hash number
+then, if you start with a first compare against the middle of the list, you
+can tell immediately whether the target hash number is greater or less than
+what you're looking for so you can then skip immediately to either the quarter-
+way point on the list, or the three-quarter, and keep doing that recursively
+until you find the target.  This is an O(log n) process, i.e., much faster than
+a linear search, but it requires the array of targets to be sorted.  qsort,
+below is the algo to do the sorting, as seen in action later on.
+
+#endif
 
 void qsort(void* v, int size, int left, int right,
       int (*comp)(void*, void*))
@@ -106,51 +180,12 @@ void qsort(void* v, int size, int left, int right,
   qsort(v, size, last + 1, right, comp);
 }	
 
-void setup() {
-  // check for the WiFi module:
+#if 0
 
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    while (true);
-  }
+The other half of the process is the binary search of the sorted list, as done
+in bsearch, also seen in use later.
 
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  // attempt to connect to Wifi network:
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);                   // print the network name (SSID);
-  delay(1000);   // wait 1 second
-  while (status != WL_CONNECTED) {
-    status = WiFi.begin(ssid, pass);
-    Serial.println("Waiting...");
-    delay(1000);   // wait 10 seconds for connection:
-  }
-
-  ip = WiFi.localIP();
-
-  server.begin();    // start the web server on port 80
-  
-  Serial.println("Connected...");
-
-  if (!SD.begin(4)) {
-    Serial.println("SD initialization failed!");
-    while (1);
-  }
-
-  parms = (parm_s *)malloc (sizeof(parm_s) * lbl_cnt);
-  for (int i = 0; i < lbl_cnt; i++) {
-    parms[i].val  = 0.0;
-    parms[i].hash = joaat.encode_str (JOAAT_STR (lbls[i]));
-    parms[i].name = lbls[i];
-  }
-
-  qsort (parms, sizeof(parm_s), 0, lbl_cnt - 1, cmp_parm);
-
-}
+#endif
 
 void *bsearch (const void *key, const void *base0,
 	       size_t nmemb, size_t size,
@@ -173,6 +208,10 @@ void *bsearch (const void *key, const void *base0,
   return (NULL);
 }
 
+
+/* a utility Javascript function that sets the values of HTML entities in
+the browser with the current parameter values known to the Arduino. */
+
 void initter (WiFiClient client, int idx)
 {
   client.println ("{");
@@ -194,26 +233,25 @@ void initter (WiFiClient client, int idx)
   client.println ("}");
 }
 
+/* The long, hairy, process of building the web page */
+
 void buildPage (WiFiClient client)
 {
   
 	    /***** scripts *******/
 
+/* scripts are Javascript routines that do stuff */
+
   client.println ("<script>");
 
-  /***
-      {
-        "name": "whatever",
-	"position": [ "dx": "val:, "dy": "val", "dz": "val",
-	   "roll": "val", "pitch": "val", "yaw": "val"],
-	"jitter": [ "dx": "val:, "dy": "val", "dz": "val"],
-	   "roll": "val", "pitch": "val", "yaw": "val"],
-	"time": [ "onset": "val:, "relax": "val", "interval": "val"]
-      }
-   ***/
+  
+  /********** showEditor *****/
 
-  // text.value = '{' + '  "name": "", + '}';
-// When the user clicks on div, open the popup
+/* formats parameter data into a decent human-readable form and lets the
+user edit it.  The edited result can be saved for later use as a again as
+a phantom-specific behaviour. */
+
+  // When the user clicks on div, open the popup
   client.println ("function showEditor() {");
   client.println ("  var popup = document.getElementById(\"myPopup\");");
   client.println ("  popup.classList.add(\"show\");");
@@ -242,6 +280,12 @@ void buildPage (WiFiClient client)
 '}';");
   client.println ("}");
 
+
+  
+  /********** window.onload *****/
+
+/* immediately the web page is built, initialise the values of HTML entities */
+
   client.println ("window.onload = function () {");
   client.println ("  let srch = window.location.search;");
   client.println ("  const searchParams = \
@@ -252,9 +296,14 @@ void buildPage (WiFiClient client)
 
   client.println ("}"); // end window.onload function
 
+
+
+  /********** updateParam *****/
+
+/* send data from the web page to the Arguino */
+  
   client.println ("function updateParam(el) {");
 
-  client.println ("console.log('in uP');");
   client.println ("const XHR = new XMLHttpRequest();");
 
   client.println ("  var text = window.location.origin + \"?update=\" + \
@@ -264,8 +313,13 @@ el.id + \"=\" + el.value;");
   client.println ("XHR.send();");
 
   client.println ("}");
-	    
+
+
 	    /**** function keyHandler(el) ****/
+
+/* The HTML mostly consists of numeric input boxes that can have up/down
+arrows clicked, or numbers pounded in followed by an "enter" keypress.  This
+handles the enter key. */
 
   client.println ("function keyHandler(el) {");
   client.println ("  if (event.keyCode=='13'){");
@@ -281,6 +335,8 @@ el.id + \"=\" + el.value;");
 	    /***** end scripts *******/
 
   /********** styles ***********/
+
+/* styles control what the HTML looks like */
 
   client.println("<style>");
 
@@ -354,12 +410,19 @@ el.id + \"=\" + el.value;");
   
   client.println("</style>");
 
+
+/* the actual page layout */
+
 	    /*** begin page ****/
 	    
   client.println ("<h1>Stewart</h2>");
 	    
 	    
 	    /****  forms ****/
+
+/* A couple of truly hairy macros that creae the numeric input boxes.
+DO NOT FIDDLE WITH THESE!  You'll wind up tearing your hair out with
+frustration and you're to young to be bald. */
 
 #define BUILD_ETY(id,lbl,stp,fm)		      \
   client.println ("<td style=\"text-align:right\">"); \
@@ -386,22 +449,6 @@ el.id + \"=\" + el.value;");
 
   client.println ("<table>");
 
-#ifdef DO_SLIDER
-  client.println ("  <tr>");
-	    
-  client.println ("  <td>");	// empty units
-  client.println ("  </td>");
-	    
-  client.println ("  <td>");	// empty id
-  client.println ("  </td>");
-	    
-  client.println ("  <td>");
-  client.println ("  <input type=\"range\" min=\"-4\" max=\"4\" \
-value=\"50\" class=\"slider\" id=\"pdxr\" form=\"position\" value=\"0\">");
-  client.println ("  </td>");
-  client.println ("  </tr>");
-
-#endif
   client.println ("  <tr>");		// start pos offset row
 
   client.println ("    <td style=\"text-align:right\">");
@@ -514,7 +561,9 @@ value=\"50\" class=\"slider\" id=\"pdxr\" form=\"position\" value=\"0\">");
 
 
 
-client.println ("<script>");
+/* Javascript to send editor data to the Arduino */
+
+  client.println ("<script>");
   client.println ("function saveText() {");
 
   client.println ("const XHR = new XMLHttpRequest();");
@@ -527,6 +576,8 @@ document.getElementById('editor').value;");
 
   client.println ("}");
  
+/* Javascript to just close the editor window */
+
   client.println ("function abandonText() {");
   client.println ("  window.location.href = window.location.origin;");
   client.println ("  return false;");
@@ -534,8 +585,8 @@ document.getElementById('editor').value;");
   client.println ("</script>");
 
 
-  // https://www.w3schools.com/js/js_ajax_http_send.asp
-  
+/* code to put the editor window and controls up */
+
    client.println ("<div class=\"popup\" \
 onclick=\"showEditor()\">Open editor");
 
@@ -580,6 +631,70 @@ id=\"submit\" />");
 	    /***** end of forms ********/
 }
 
+#if 0
+
+Normal C/C++ code has one entry point, called main(), where execution starts.
+Arduino code as two such entry points, setup() and loop().  setup() is run once
+to, guess what, set things up.  loop(), guess what again, loops forever.
+
+#endif
+
+void setup()
+{
+
+  int status = WL_IDLE_STATUS;
+
+  // check for the WiFi module:
+
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);                   // print the network name (SSID);
+  delay(1000);   // wait 1 second
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
+    Serial.println("Waiting...");
+    delay(1000);   // wait 10 seconds for connection:
+  }
+
+  ip = WiFi.localIP();
+
+  server.begin();    // start the web server on port 80
+  
+  Serial.println("Connected...");
+
+#if 0
+  if (!SD.begin(4)) {
+    Serial.println("SD initialization failed!");
+    while (1);
+  }
+#endif
+
+/* create the parameter array and compute the hash values */
+
+  parms = (parm_s *)malloc (sizeof(parm_s) * lbl_cnt);
+  for (int i = 0; i < lbl_cnt; i++) {
+    parms[i].val  = 0.0;
+    parms[i].hash = joaat.encode_str (JOAAT_STR (lbls[i]));
+    parms[i].name = lbls[i];
+  }
+
+/* sort the array by hash value */
+
+  qsort (parms, sizeof(parm_s), 0, lbl_cnt - 1, cmp_parm);
+
+}
+
 
               
 void loop() {
@@ -592,9 +707,11 @@ void loop() {
       if (client.available()) {     // if there's bytes to read from the client,
 	
         char c = client.read();     // read a byte, then
-	Serial.print (c);
+	//	Serial.print (c);
         if (c == '\n') {            // if the byte is a newline character
           if (currentLine.length() == 0) {
+	    // if it's blank line, no more input and go ahead and build the
+	    // page.
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println();
@@ -607,28 +724,35 @@ void loop() {
 	  else {	// non-empty line
 	    //	    Serial.println (currentLine);
 	    if (currentLine.startsWith("POST")) {
-	      Serial.println ("doing post");
+	      // if it's incoming data, figure out what kind of data it is...
 	      String startString = "update=";
 	      int startPos = currentLine.indexOf(startString);
 	      if (-1 != startPos) {
-		Serial.println ("got startpos");
+		// if it's parameter update data...
 		startPos += startString.length ();
 		String endString = "HTTP";
 		int endPos = currentLine.indexOf(endString);
 		if (-1 != endPos) {
-		  Serial.println ("got endpos");
+		  // isolate the actual data
 		  endPos--;
 		  String text = currentLine.substring (startPos, endPos);
+		  // and then split up up into parameter name
+		  // and parameter value
 		  int endPos =  text.indexOf ("=");
 		  String vbl = text.substring (0, endPos);
 		  String vals = text.substring (1 + endPos);
 		  double val = vals.toDouble ();
 
+		  // compute the hash of the name...
 		  uint32_t hash = joaat.encode_str (JOAAT_STR (vbl.c_str ()));
+		  // and search for it
 		  void *res = bsearch (reinterpret_cast<void *>(hash), parms,
 			 lbl_cnt, sizeof(parm_s), cmp_parm_str);
 		  if (res != nullptr) {
+		    // if you find it, update the value
 		    ((parm_s *)res)->val = val;
+		    Serial.println (vbl + " = "
+				    + String (((parm_s *)res)->val, 2));
 		  }
 		}
 	      }
